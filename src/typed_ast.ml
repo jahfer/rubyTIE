ff2d602b90e191c642a7e16e0c9b85b4114b1028
Jahfer Husain <jahfer.husain@shopify.com>
2018-03-13 07:41:59 -0400

    Look up existing references before annotating type

--- src/typed_ast.ml
+++ src/typed_ast.ml
@@ -24,10 +24,10 @@ type typed_core_expression = {
 let current_var = ref 1
 let gen_fresh_t () =
   let tv = !current_var in
-  incr current_var; TPoly(Core.sprintf "v/1%03i" tv)
+  incr current_var; TPoly(Core.sprintf "t/1%03i" tv)
 
 module AnnotationMap = Map.Make (String)
-let annotations = AnnotationMap.empty
+let annotations = ref AnnotationMap.empty
 
 let rec eval core_expr = core_expr |> annotate (* |> constrain_types *)
 
@@ -54,23 +54,18 @@ and constrain_types { expr_loc; expr_desc; expr_type } =
       let { expr_type } = eval expr in expr_type
   in { expr_loc; expr_desc; expr_type = typ; level = 2 }
 
-and annotate ({ expr_loc; expr_desc } : core_expression) : typed_core_expression =
+and annotate ({ expr_loc; expr_desc } : core_expression) =
   let typ = match expr_desc with
+  | ExprValue (v) -> gen_fresh_t ()
+  | ExprCall  (_) -> gen_fresh_t () (* TODO: look up method in object table *)
+  | ExprBody (_, expr) -> let { expr_type } = annotate expr in expr_type
   | ExprVar (name, _) | ExprIVar (name, _) | ExprConst ((name, _), _)
   | ExprAssign (name, _) | ExprIVarAssign (name, _) | ExprConstAssign (name, _)
   | ExprFunc (name, _, _) ->
-    begin match AnnotationMap.find_opt name annotations with
+    begin match AnnotationMap.find_opt name !annotations with
       | Some(typ) -> typ
-      | None -> gen_fresh_t ()
+      | None -> let gen_typ = gen_fresh_t () in
+        annotations := AnnotationMap.add name gen_typ !annotations;
+        gen_typ
     end
-  | ExprCall ({ expr_desc } as receiver, meth, _) ->
-    begin match expr_desc with
-      | ExprVar (name, _) | ExprIVar (name, _) | ExprConst ((name, _), _) ->
-      begin match AnnotationMap.find_opt name annotations with
-        | Some(typ) -> typ
-        | None -> gen_fresh_t ()
-      end
-      | _ -> let { expr_type } = annotate receiver in expr_type
-    end
-  | _ -> TAny
   in { expr_loc; expr_desc; expr_type = typ; level = 1 }
